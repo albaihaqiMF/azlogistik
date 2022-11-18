b1c5ae7d44478110b0b24fa9673878fb599b7dd9
Muhammad Fahmi Albaihaqi <fhmalba@gmail.com>
2022-11-18 14:02:31 +0700

    first commit

---
+++ resources/views/question/button.blade.php
@@ -0,0 +1,12 @@
+<x-guest-layout title="Button">
+    <div class="w-full h-screen flex justify-center items-center">
+        <button class=" border-sky-500 rounded-full px-[24px] py-[8px] flex space-x-2 border-2 title" title="Hello World">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 5.00024V19.0002" stroke="#176CF7" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" />
+                <path d="M5 12H19" stroke="#176CF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <span>Button</span>
+        </button>
+    </div>
+</x-guest-layout>
---
+++ resources/views/question/movie.blade.php
@@ -0,0 +1,5 @@
+<x-guest-layout title="">
+    <div class="p-4">
+        <livewire:movie.index />
+    </div>
+</x-guest-layout>
